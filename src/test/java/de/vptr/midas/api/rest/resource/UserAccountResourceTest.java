05a7a26cea7e48c20ef37c64b27abadf5789f0bf
Gregor Dietrich <gregor@dietrich.lan>
2025-07-20 00:27:40 +0200

    implement authorization tests for user accounts and ranks

--- src/test/java/de/vptr/midas/api/rest/resource/UserAccountResourceTest.java
+++ src/test/java/de/vptr/midas/api/rest/resource/UserAccountResourceTest.java
@@ -1,10 +1,13 @@
 package de.vptr.midas.api.rest.resource;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.is;
 
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
 
 @QuarkusTest
 class UserAccountResourceTest {
@@ -16,6 +19,113 @@ class UserAccountResourceTest {
         given()
         .when()
             .get(ENDPOINT_URL)
+        .then()
+            .statusCode(401);
+        // @formatter:on
+    }
+
+    @Test
+    void testGetAllUserAccounts_authorized() {
+        // @formatter:off
+        given()
+            .auth().basic("admin", "admin")
+        .when()
+            .get(ENDPOINT_URL)
+        .then()
+            .statusCode(200)
+            .contentType(ContentType.JSON);
+        // @formatter:on
+    }
+
+    @Test
+    void testGetUserAccountById_unauthorized() {
+        // @formatter:off
+        given()
+        .when()
+            .get(ENDPOINT_URL + "/1")
+        .then()
+            .statusCode(401);
+        // @formatter:on
+    }
+
+    @Test
+    void testGetUserAccountById_authorized() {
+        // @formatter:off
+        given()
+            .auth().basic("admin", "admin")
+        .when()
+            .get(ENDPOINT_URL + "/1")
+        .then()
+            .statusCode(anyOf(is(200), is(404)));
+        // @formatter:on
+    }
+
+    @Test
+    void testCreateUserAccount_unauthorized() {
+        final String accountJson = """
+                {
+                    "userId": 1,
+                    "accountType": "test",
+                    "accountValue": "value"
+                }
+                """;
+        // @formatter:off
+        given()
+            .contentType(ContentType.JSON)
+            .body(accountJson)
+        .when()
+            .post(ENDPOINT_URL)
+        .then()
+            .statusCode(401);
+        // @formatter:on
+    }
+
+    @Test
+    void testCreateUserAccount_authorizedButInsufficientRole() {
+        final String accountJson = """
+                {
+                    "userId": 1,
+                    "accountType": "test",
+                    "accountValue": "value"
+                }
+                """;
+        // @formatter:off
+        given()
+            .auth().basic("admin", "admin")
+            .contentType(ContentType.JSON)
+            .body(accountJson)
+        .when()
+            .post(ENDPOINT_URL)
+        .then()
+            .statusCode(anyOf(is(201), is(403)));
+        // @formatter:on
+    }
+
+    @Test
+    void testUpdateUserAccount_unauthorized() {
+        final String accountJson = """
+                {
+                    "accountType": "updated",
+                    "accountValue": "updatedValue"
+                }
+                """;
+        // @formatter:off
+        given()
+            .contentType(ContentType.JSON)
+            .body(accountJson)
+        .when()
+            .put(ENDPOINT_URL + "/1")
+        .then()
+            .statusCode(401);
+        // @formatter:on
+    }
+
+    @Test
+    void testDeleteUserAccount_unauthorized() {
+        // @formatter:off
+        given()
+        .when()
+            .delete(ENDPOINT_URL + "/1")
         .then()
             .statusCode(401);
         // @formatter:on
--- src/test/java/de/vptr/midas/api/rest/resource/UserRankResourceTest.java
+++ src/test/java/de/vptr/midas/api/rest/resource/UserRankResourceTest.java
@@ -1,10 +1,13 @@
 package de.vptr.midas.api.rest.resource;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.is;
 
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
 
 @QuarkusTest
 class UserRankResourceTest {
@@ -16,6 +19,110 @@ class UserRankResourceTest {
         given()
         .when()
             .get(ENDPOINT_URL)
+        .then()
+            .statusCode(401);
+        // @formatter:on
+    }
+
+    @Test
+    void testGetAllUserRanks_authorized() {
+        // @formatter:off
+        given()
+            .auth().basic("admin", "admin")
+        .when()
+            .get(ENDPOINT_URL)
+        .then()
+            .statusCode(200)
+            .contentType(ContentType.JSON);
+        // @formatter:on
+    }
+
+    @Test
+    void testGetUserRankById_unauthorized() {
+        // @formatter:off
+        given()
+        .when()
+            .get(ENDPOINT_URL + "/1")
+        .then()
+            .statusCode(401);
+        // @formatter:on
+    }
+
+    @Test
+    void testGetUserRankById_authorized() {
+        // @formatter:off
+        given()
+            .auth().basic("admin", "admin")
+        .when()
+            .get(ENDPOINT_URL + "/1")
+        .then()
+            .statusCode(anyOf(is(200), is(404)));
+        // @formatter:on
+    }
+
+    @Test
+    void testCreateUserRank_unauthorized() {
+        final String rankJson = """
+                {
+                    "name": "Test Rank",
+                    "userAdd": true
+                }
+                """;
+        // @formatter:off
+        given()
+            .contentType(ContentType.JSON)
+            .body(rankJson)
+        .when()
+            .post(ENDPOINT_URL)
+        .then()
+            .statusCode(401);
+        // @formatter:on
+    }
+
+    @Test
+    void testCreateUserRank_authorizedButInsufficientRole() {
+        final String rankJson = """
+                {
+                    "name": "Test Rank",
+                    "userAdd": true
+                }
+                """;
+        // @formatter:off
+        given()
+            .auth().basic("admin", "admin")
+            .contentType(ContentType.JSON)
+            .body(rankJson)
+        .when()
+            .post(ENDPOINT_URL)
+        .then()
+            .statusCode(anyOf(is(201), is(403)));
+        // @formatter:on
+    }
+
+    @Test
+    void testUpdateUserRank_unauthorized() {
+        final String rankJson = """
+                {
+                    "name": "Updated Rank"
+                }
+                """;
+        // @formatter:off
+        given()
+            .contentType(ContentType.JSON)
+            .body(rankJson)
+        .when()
+            .put(ENDPOINT_URL + "/1")
+        .then()
+            .statusCode(401);
+        // @formatter:on
+    }
+
+    @Test
+    void testDeleteUserRank_unauthorized() {
+        // @formatter:off
+        given()
+        .when()
+            .delete(ENDPOINT_URL + "/1")
         .then()
             .statusCode(401);
         // @formatter:on
